42e99b73793346261a9bc16afc8f0d013a40d461
Fekade Senbeta <fekadesenbeta@gmail.com>
2020-04-11 18:37:15 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const tasks = document.getElementById("tasks");
+  const form = document.getElementById("create-task-form");
+  const input = document.getElementById("new-task-description");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const description = formData.get("new-task-description");
+    const color = formData.get("color");
+    const taskList = new TaskList(description, color);
+    tasks.appendChild(taskList.render());
+    input.value = "";
+  });
 });
